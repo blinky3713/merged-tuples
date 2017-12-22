f27d2db7ce4738e9900add53f7d01401e8c9e1c6
Blinky <blinky3713@gmail.com>
2017-12-22 14:46:25 -0800

    HListRep

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,6 +12,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Lib
     ( someFunc
@@ -20,15 +21,48 @@ module Lib
 import Data.Kind
 import Data.Tagged (Tagged(..))
 import Data.Proxy (Proxy(..))
+import qualified GHC.Generics as GHC (Generic)
+import Generics.SOP
 import GHC.TypeLits (CmpNat, Nat)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
--- [Tagged "b" b, Tagged "c" c, Tagged "a" a, Tagged "d" d]
--- Proxy [("a", 1), ("b", 2), ("c", 3), ("d", 4)]
--- [Tagged "a" a, Tagged b "b", Tagged "c" c, Tagged "d" d]
+-- | HList
+data HList :: [*] -> * where
+  HNil :: HList '[]
+  (:<) :: a -> HList as -> HList (a : as)
 
+infixr 0 :<
+
+instance Show (HList '[]) where
+  show _ = "HNil"
+
+instance (Show a, Show (HList as)) => Show (HList (a : as)) where
+  show (a :< as) = show a ++ " :< " ++ show as
+
+-- | Generic representation to HList representation
+class HListRep a xs | a -> xs where
+  toHList :: a -> HList xs
+  fromHList :: HList xs -> a
+
+instance HListRep (NP I '[]) '[] where
+  toHList _ = HNil
+  fromHList _ = Nil
+
+instance HListRep (NP I as) as => HListRep (NP I (a:as)) (a:as) where
+  toHList (I a :* rest) = a :< toHList rest
+  fromHList (a :< rest) = I a :* fromHList rest
+
+instance HListRep (NP f as') as => HListRep (NS (NP f) '[as']) as where
+  toHList (Z rep) = toHList rep
+  fromHList = Z . fromHList
+
+instance HListRep (NS (NP f) as') as => HListRep (SOP f as') as where
+  toHList (SOP rep) = toHList rep
+  fromHList = SOP . fromHList
+
+-- | Sort a Tagged HList
 class Sort (xs :: [*]) where
   type Sort' xs :: [*]
   sort :: HList xs -> HList (Sort' xs)
@@ -65,12 +99,8 @@ instance Insert x ys => InsertCmp 'GT x y ys where
   type InsertCmp' 'GT x y ys = y : Insert' x ys
   insertCmp _ x y ys = y :< insert x ys
 
-data HList :: [*] -> * where
-  HNil :: HList '[]
-  (:<) :: a -> HList as -> HList (a : as)
 
-infixr 0 :<
-
+-- | Unwrap all the Tagged items in an HList
 class UnTag t ut | t -> ut where
   unTag :: HList t -> HList ut
 
@@ -80,8 +110,10 @@ instance UnTag '[] '[] where
 instance UnTag ts uts => UnTag (Tagged n a : ts) (a : uts) where
   unTag (Tagged a :< ts) = a :< unTag ts
 
-as :: HList '[Tagged 2 String, Tagged 1 Int, Tagged 3 Bool]
+
+-- | example
+as :: HList '[Tagged 3 String, Tagged 1 Int, Tagged 2 Bool]
 as = Tagged "hello" :< Tagged 0 :< Tagged False :< HNil
 
-bs :: HList '[Int, String, Bool]
+bs :: HList '[Int, Bool, String]
 bs = unTag . sort $ as
